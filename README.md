4eb21360ae59f59143b36e2d3ed31739ebea9a53
ZaibUn-Nisa <157270149+ZaibUn-Nisa@users.noreply.github.com>
2024-02-01 03:15:04 +0500

    initial commit
    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# sqf_lite_flutter
- sqflite_CRUD
+# Destiny Diverge 🤔
+
+## Our Goal
+
+In this challenge, you will be building a choose your own adventure game. In the process, you should apply your knowledge from the last module (Quizz_app) to use Dart OOP principles to build a well organised project.
+
+![Destini](https://github.com/hinaabbaskhan/destiny-diverge/assets/47082795/38e06534-d470-4a00-be98-a78f8522b59b)
+
---
+++ lib/story.dart
@@ -0,0 +1,19 @@
+//TODO: Step 2 - Create a new class called Story.
+class Story {
+  String? storyTitle;
+  String? choiceOne;
+  String? choice2;
+
+  Story(
+    String s,
+    c1,
+    c2,
+  ) {
+    storyTitle = s;
+    choiceOne = c1;
+    choice2 = c2;
+  }
+}
+//TODO: Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
+
+//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
